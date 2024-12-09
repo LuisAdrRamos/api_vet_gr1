eed1f5bac66bae3c8f9d1d3814fa9fdf84248c67
Luis Adrian Ramos <luchoramos042004@gmail.com>
2024-12-09 14:43:49 -0500

    verificacion de correo

--- src/controllers/veterinario_controller.js
+++ src/controllers/veterinario_controller.js
@@ -1,20 +1,18 @@
-import veterinario from "../models/veterinario.js"
+import Veterinario from "../models/veterinario.js"
+import sendMailToUser from "../config/nodemailer.js"
 
-const registro = async (req, res) => {
-    const {email, password} = req.body
-    
-    if (Object.values(req.body).includes("")) return res.status(400).json({msg:"lo sentimos debes llenar todo los campos"})
-    
-    const verificarEmail = await veterinario.findOne({email})
-    if (verificarEmail) return res.status(400).json({msg:"lo sentimos este email ya esta registrado"})
-
-    const nuevoVeterinario = new veterinario(req.body)
+const registro = async (req,res)=>{
+    const {email,password} = req.body
+    if (Object.values(req.body).includes("")) return res.status(400).json({msg:"Lo sentimos, debes llenar todos los campos"})
+    const verificarEmailBDD = await Veterinario.findOne({email})
+    if(verificarEmailBDD) return res.status(400).json({msg:"Lo sentimos, el email ya se encuentra registrado"})
+    const nuevoVeterinario = new Veterinario(req.body)
     nuevoVeterinario.password = await nuevoVeterinario.encrypPassword(password)
-    nuevoVeterinario.crearToken()
+
+    const token = nuevoVeterinario.crearToken()
+    await sendMailToUser(email,token)
     await nuevoVeterinario.save()
-    res.status(200).json(nuevoVeterinario)
-    
-    res.send("Veterinario Registrado")
+    res.status(200).json({msg:"Revisa tu correo electrónico para confirmar tu cuenta"})
 }
 
 export {
