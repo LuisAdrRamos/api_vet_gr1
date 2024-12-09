57ed18b2c69c289e35ea430ab31e521ec02f4dc8
Luis Adrian Ramos <luchoramos042004@gmail.com>
2024-12-09 15:50:06 -0500

    login

--- src/controllers/veterinario_controller.js
+++ src/controllers/veterinario_controller.js
@@ -1,5 +1,6 @@
 import Veterinario from "../models/veterinario.js"
 import sendMailToUser from "../config/nodemailer.js"
+import veterinario from "../models/veterinario.js"
 
 const registro = async (req,res)=>{
     const {email,password} = req.body
@@ -15,6 +16,39 @@ const registro = async (req,res)=>{
     res.status(200).json({msg:"Revisa tu correo electrónico para confirmar tu cuenta"})
 }
 
+const confirmEmail = async (req, res) => {
+    const {token} = req.params
+    if (!(token)) return res.status(400).json({msg:"Lo sentimos o se puede validar su cuenta"})
+    
+    const veterinarioBDD = await Veterinario.findOne({token})
+    if(!(veterinarioBDD?.token)) return res.status(400).json({msg:"La centa ya ha sido confirmada"})
+    
+    veterinarioBDD.token = null
+    veterinarioBDD.confirmEmail = true
+    await veterinarioBDD.save()
+
+    res.status(200).json({msg:"Token confirmado, ya puedes iniciar sesion"})
+}
+
+const login = async (req, res) => {
+    const {email, password} = req.body
+    
+    if (Object.values(req.body).includes("")) return res.status(400).json({msg:"Lo sentimos, debes llenar todos los campos"})
+    
+    const veterinarioBDD = await Veterinario.findOne({email})
+    if (veterinarioBDD?.confirmEmail===false) return res.status(400).json({msg:"Los sentimos debe verificar la cuenta"})
+    
+    if (!veterinarioBDD) return res.status(400).json({msg:"Lo sentimos, email incorrecto"})    
+    const verificarPass = await veterinarioBDD.matchPassword(password)
+    if (!verificarPass) return res.status(400).json({msg:"Lo sentimos, contraseña incorrecta"})
+    
+    res.status(200).json(veterinarioBDD)
+}
+
+
+
 export {
-    registro
+    registro,
+    confirmEmail,
+    login
 }
--- src/routers/veterinario_routers.js
+++ src/routers/veterinario_routers.js
@@ -1,7 +1,9 @@
 import { Router } from "express";
-import { registro } from "../controllers/veterinario_controller.js";
+import { confirmEmail, login, registro } from "../controllers/veterinario_controller.js";
 const router = Router()
 
 router.post('/registro', registro)
+router.get('/confirmar/:token', confirmEmail)
+router.post('/login', login)
 
 export default router
