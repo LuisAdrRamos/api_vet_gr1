2feba6e8e8e7947bd2ff6bf811312ec2ec04038b
Luis Adrian Ramos <luchoramos042004@gmail.com>
2024-12-12 15:25:42 -0500

    perfil vet

--- src/controllers/veterinario_controller.js
+++ src/controllers/veterinario_controller.js
@@ -1,6 +1,7 @@
 import Veterinario from "../models/veterinario.js"
-import sendMailToUser from "../config/nodemailer.js"
+import { sendMailToUser, sendMailToRecoveryPassword } from "../config/nodemailer.js"
 import veterinario from "../models/veterinario.js"
+import {generarJWT} from "../helpers/crearJWT.js"
 
 const registro = async (req,res)=>{
     const {email,password} = req.body
@@ -11,7 +12,7 @@ const registro = async (req,res)=>{
     nuevoVeterinario.password = await nuevoVeterinario.encrypPassword(password)
 
     const token = nuevoVeterinario.crearToken()
-    await sendMailToUser(email,token)
+    await spsndMailToUser(email,token)
     await nuevoVeterinario.save()
     res.status(200).json({msg:"Revisa tu correo electrónico para confirmar tu cuenta"})
 }
@@ -42,13 +43,67 @@ const login = async (req, res) => {
     const verificarPass = await veterinarioBDD.matchPassword(password)
     if (!verificarPass) return res.status(400).json({msg:"Lo sentimos, contraseña incorrecta"})
     
-    res.status(200).json(veterinarioBDD)
+    const {nombre, apellido, telefono, direccion} = veterinarioBDD
+    const tokenJWT = generarJWT(veterinarioBDD._id, "veterinario"); 
+    res.status(200).json({
+        tokenJWT,
+        nombre,
+        apellido,
+        telefono,
+        direccion
+    });
 }
 
+const recuperarPassword = async(req,res)=>{
+    const {email} = req.body
+    if (Object.values(req.body).includes("")) return res.status(404).json({msg:"Lo sentimos, debes llenar todos los campos"})
+    
+        const veterinarioBDD = await Veterinario.findOne({email})
+    if(!veterinarioBDD) return res.status(404).json({msg:"Lo sentimos, el usuario no se encuentra registrado"})
+    
+    const token = veterinarioBDD.crearToken()
+    veterinarioBDD.token=token
+    await sendMailToRecoveryPassword(email,token)
+    await veterinarioBDD.save()
+    res.status(200).json({msg:"Revisa tu correo electrónico para reestablecer tu cuenta"})
+}
+
+const tokenPassRecover = async(req,res) => {
+    const {token} = req.params
+    if (!(token)) return res.status(400).json({msg:"Lo sentimos no se puede validar su cuenta"})
+    
+    const veterinarioBDD = await Veterinario.findOne({token})
+    if(veterinarioBDD?.token !== token) res.status(400).json({msg:"Lo sentimos, no se puede validar su token"})
+    
+    await veterinarioBDD.save()
+    res.status(200).json({msg:"Token confirmado, ya puedes crear tu nuevo password"}) 
+}
+
+const newPassword = async(req,res) => {
+    const {password, confirmPassword} = req.body
+    if (Object.values(req.body).includes("")) return res.status(404).json({msg:"Lo sentimos, debes llenar todos los campos"})
+    
+    if (password != confirmPassword) return res.status(404).json({msg:"Las contraseñas no coinciden"})
+    const veterinarioBDD = await Veterinario.findOne({token:req.params.token})
+    if(veterinarioBDD?.token !== req.params.token) return res.status(404).json({msg:"Lo sentimos, no se puede validar la cuenta"})
+
+    veterinarioBDD.token = null
+    veterinarioBDD.password = await veterinarioBDD.encrypPassword(password)
+    await veterinarioBDD.save()
+    res.status(200).json({msg:"Contraseña cambiada exitosamente"})
+}
+
+const perfilUser = (req, res) => {
+  res.send("Perfil del usuario")
+}
 
 
 export {
     registro,
     confirmEmail,
-    login
+    login,
+    recuperarPassword,
+    tokenPassRecover,
+    newPassword,
+    perfilUser
 }
--- src/routers/veterinario_routers.js
+++ src/routers/veterinario_routers.js
@@ -1,9 +1,15 @@
 import { Router } from "express";
-import { confirmEmail, login, registro } from "../controllers/veterinario_controller.js";
+import { confirmEmail, login, newPassword, perfilUser, recuperarPassword, registro, tokenPassRecover } from "../controllers/veterinario_controller.js";
+import { verificarAutentificacion } from "../helpers/crearJWT.js";
 const router = Router()
 
 router.post('/registro', registro)
 router.get('/confirmar/:token', confirmEmail)
 router.post('/login', login)
+router.post('/recuperar-password', recuperarPassword)
+router.post('/recuperar-password/:token', tokenPassRecover)
+router.post('/nueva-password/:token', newPassword)
+
+router.get('/perfil', verificarAutentificacion, perfilUser)
 
 export default router
